62604ac013c76281e983b380d8e504b7cef1ccdf
alifya.difa707 <alifya.difa707@gmail.com>
2022-06-01 19:00:52 +0700

    Fix bug

--- src/layouts/header/Toolbar.vue
+++ src/layouts/header/Toolbar.vue
@@ -3,12 +3,7 @@
     :class="['toolbar bg-white py-4 px-4', { 'shadow-sm': offsetContent }]"
     style="z-index: 1"
   >
-    <template v-if="route.name === 'mythings-application'">
-      <ApplicationShow />
-    </template>
-    <template v-else>
-      <MainToolbar />
-    </template>
+    <MainToolbar />
   </div>
 </template>
 
@@ -16,13 +11,11 @@
 import { defineComponent, onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 import MainToolbar from "@/layouts/toolbars/Main.vue";
-import ApplicationShow from "@/layouts/toolbars/show/Application.vue";
 
 export default defineComponent({
   name: "toolbar",
   components: {
     MainToolbar,
-    ApplicationShow,
   },
   setup() {
     const route = useRoute();
